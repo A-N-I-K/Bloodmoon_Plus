90bf629dea03b7d00225dbf5fc8508c5e816c072
Anik <A.N.I.K@live.com>
2021-06-23 02:10:41 -0400

    Random BM intervals

--- src/main/java/org/spectralmemories/bloodmoon/PeriodicNightCheck.java
+++ src/main/java/org/spectralmemories/bloodmoon/PeriodicNightCheck.java
@@ -1,5 +1,10 @@
 package org.spectralmemories.bloodmoon;
 
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
@@ -10,256 +15,211 @@ import org.spectralmemories.sqlaccess.FieldType;
 import org.spectralmemories.sqlaccess.SQLAccess;
 import org.spectralmemories.sqlaccess.SQLField;
 
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
+public class PeriodicNightCheck implements Runnable, Listener {
 
+	public static final int DAY = 24000;
 
-public class PeriodicNightCheck implements Runnable, Listener
-{
+	private static Map<World, PeriodicNightCheck> nightChecks;
 
-    public static final int DAY = 24000;
+	private long checkupAfter;
+	private int daysBeforeBloodMoon;
 
-    private static Map<World, PeriodicNightCheck> nightChecks;
+	private World world;
+	private BloodmoonActuator actuator;
 
-    private long checkupAfter;
-    private int daysBeforeBloodMoon;
+	public PeriodicNightCheck(World world, BloodmoonActuator actuator) {
+		ConfigReader configReader = Bloodmoon.GetInstance().getConfigReader(world);
+		this.actuator = actuator;
+		this.world = world;
+		daysBeforeBloodMoon = (configReader.GetIntervalConfig() - 1); // Workaround
+		checkupAfter = world.getFullTime();
 
-    private World world;
-    private BloodmoonActuator actuator;
+		AddCheck(this);
+	}
 
-    public PeriodicNightCheck(World world, BloodmoonActuator actuator)
-    {
-        ConfigReader configReader = Bloodmoon.GetInstance().getConfigReader(world);
-        this.actuator = actuator;
-        this.world = world;
-        daysBeforeBloodMoon = (configReader.GetIntervalConfig() - 1); //Workaround
-        checkupAfter = world.getFullTime();
+	private static void AddCheck(PeriodicNightCheck instance) {
+		if (nightChecks == null)
+			nightChecks = new HashMap<>();
 
-        AddCheck(this);
-    }
+		nightChecks.put(instance.GetWorld(), instance);
+	}
 
-    private static void AddCheck (PeriodicNightCheck instance)
-    {
-        if (nightChecks == null) nightChecks = new HashMap<>();
+	public static int GetDaysRemaining(World world) {
+		PeriodicNightCheck instance = nightChecks.get(world);
 
-        nightChecks.put(instance.GetWorld(), instance);
-    }
+		if (instance != null)
+			return (instance.GetRemainingDays() + 1);
 
-    public static int GetDaysRemaining (World world)
-    {
-        PeriodicNightCheck instance = nightChecks.get(world);
+		return -1;
+	}
 
-        if (instance != null) return (instance.GetRemainingDays() + 1);
+	public World GetWorld() {
+		return world;
+	}
 
-        return -1;
-    }
+	public static PeriodicNightCheck GetPeriodicNightCheck(World world) {
+		try {
+			return nightChecks.get(world);
+		} catch (Exception ignored) {
+		}
+		return null;
+	}
 
-    public World GetWorld ()
-    {
-        return world;
-    }
+	public int GetBloodMoonInterval() {
+		Random r = new Random();
+		return r.nextInt(Bloodmoon.GetInstance().getConfigReader(world).GetIntervalConfig() - 1) + 1;
+		// return Bloodmoon.GetInstance().getConfigReader(world).GetIntervalConfig();
+	}
 
-    public static PeriodicNightCheck GetPeriodicNightCheck (World world)
-    {
-        try
-        {
-            return nightChecks.get(world);
-        }
-        catch (Exception ignored){}
-        return null;
-    }
+	public int GetRemainingDays() {
+		return daysBeforeBloodMoon;
+	}
 
-    public int GetBloodMoonInterval ()
-    {
-        return Bloodmoon.GetInstance().getConfigReader(world).GetIntervalConfig();
-    }
+	public void SetDaysRemaining(int remaining) {
+		daysBeforeBloodMoon = remaining;
+	}
 
-    public int GetRemainingDays ()
-    {
-        return daysBeforeBloodMoon;
-    }
+	public void SetCheckAfter(long time) {
+		checkupAfter = time;
+	}
 
-    public void SetDaysRemaining (int remaining)
-    {
-        daysBeforeBloodMoon = remaining;
-    }
+	public long GetCheckAfter() {
+		return checkupAfter;
+	}
 
-    public void SetCheckAfter (long time)
-    {
-        checkupAfter = time;
-    }
+	public void UpdateCacheDatabase() {
 
-    public long GetCheckAfter ()
-    {
-        return checkupAfter;
-    }
+		String worldUid = world.getUID().toString();
+		String tableName = "lastBloodMoon";
+		SQLAccess access = Bloodmoon.GetInstance().getSqlAccess();
+		boolean exists = false;
+		try {
+			exists = access.EntryExist(tableName, new SQLField("world", FieldType.TEXT, true, false), worldUid);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
-    public void UpdateCacheDatabase ()
-    {
+		String sql;
+		int days = (actuator.isInProgress()) ? -1 : daysBeforeBloodMoon;
+		long checkAt = (actuator.isInProgress()) ? 0 : checkupAfter;
+		if (exists) {
+			sql = "UPDATE " + tableName + " SET days = " + (days + 1) + ", checkAt = " + checkAt;
+			sql += " WHERE world = '" + worldUid + "';";
+		} else {
+			sql = "INSERT INTO " + tableName + " VALUES('" + worldUid + "', " + (days + 1) + ", " + checkAt + ");";
+		}
 
-        String worldUid = world.getUID().toString();
-        String tableName = "lastBloodMoon";
-        SQLAccess access = Bloodmoon.GetInstance().getSqlAccess();
-        boolean exists = false;
-        try
-        {
-            exists = access.EntryExist (tableName, new SQLField(
-                            "world",
-                            FieldType.TEXT,
-                            true,
-                            false), worldUid);
-        }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-        }
+		try {
+			if (!access.ExecuteSQLOperation(sql)) {
+				System.out.println("[Warning] There was an issue updating plugin");
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
 
-        String sql;
-        int days = (actuator.isInProgress()) ? -1 : daysBeforeBloodMoon;
-        long checkAt = (actuator.isInProgress()) ? 0 : checkupAfter;
-        if (exists)
-        {
-            sql = "UPDATE " + tableName + " SET days = " + (days + 1) + ", checkAt = " + checkAt;
-            sql += " WHERE world = '" + worldUid + "';";
-        }
-        else
-        {
-            sql = "INSERT INTO " + tableName + " VALUES('" + worldUid + "', " + (days + 1) + ", " + checkAt + ");";
-        }
+	}
 
-        try
-        {
-            if (! access.ExecuteSQLOperation(sql))
-            {
-                System.out.println("[Warning] There was an issue updating plugin");
-            }
-        }
-        catch (SQLException e)
-        {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-        }
+	@Override
+	public void run() {
+		Bloodmoon.GetInstance().GetScheduler().runTaskLater(Bloodmoon.GetInstance(), this, Bloodmoon.NIGHT_CHECK_DELAY);
 
+		/*
+		 * 
+		 * This code is quite messy honestly. A complete rewrite of the class should be
+		 * planned
+		 * 
+		 * but, it works, so for this alpha version, this is fine
+		 * 
+		 */
 
-    }
+		Check11();
+		Check13();
+		CheckDay();
+	}
 
-    @Override
-    public void run()
-    {
-        Bloodmoon.GetInstance().GetScheduler().runTaskLater(Bloodmoon.GetInstance(), this, Bloodmoon.NIGHT_CHECK_DELAY);
+	private void Check11() {
+		LocaleReader localeReader = Bloodmoon.GetInstance().getLocaleReader();
+		if (world.getTime() > 11000) {
+			if (world.getFullTime() <= checkupAfter)
+				return;
+			if (daysBeforeBloodMoon > 0) {
 
-        /*
+				if (daysBeforeBloodMoon == 1) {
+					LocaleReader.MessageAllLocale("BloodMoonTomorrow", null, null, world);
+				} else {
+					LocaleReader.MessageAllLocale("DaysBeforeBloodMoon", new String[] { "$d" },
+							new String[] { String.valueOf(daysBeforeBloodMoon) }, world);
+				}
+				SetDaysRemaining(daysBeforeBloodMoon - 1);
+				checkupAfter = getNextEvening();
+				return;
+			}
 
-        This code is quite messy honestly. A complete rewrite of the class should be planned
+			// Day 0: prepare for Blood Moon
+			LocaleReader.MessageAllLocale("BloodMoonTonight", null, null, world);
+			checkupAfter = getTodayZero() + 12000;
+		}
+	}
 
-        but, it works, so for this alpha version, this is fine
+	private void Check13() {
+		LocaleReader localeReader = Bloodmoon.GetInstance().getLocaleReader();
+		ConfigReader configReader = Bloodmoon.GetInstance().getConfigReader(world);
+		// Check if its Blood Moon night, then time is over 13000, and if its the day
+		// after the day 0 warning
+		if (world.getFullTime() >= checkupAfter && world.getTime() >= 12000 && daysBeforeBloodMoon == 0) {
 
-         */
+			actuator.StartBloodMoon();
 
-        Check11();
-        Check13();
-        CheckDay();
-    }
+			checkupAfter = getNextEvening();
+			daysBeforeBloodMoon = (configReader.GetIntervalConfig() - 1);
+		}
+	}
 
-    private void Check11 ()
-    {
-        LocaleReader localeReader = Bloodmoon.GetInstance().getLocaleReader();
-        if (world.getTime() > 11000)
-        {
-            if (world.getFullTime() <= checkupAfter) return;
-            if (daysBeforeBloodMoon > 0)
-            {
+	private void CheckDay() {
+		LocaleReader localeReader = Bloodmoon.GetInstance().getLocaleReader();
+		ConfigReader configReader = Bloodmoon.GetInstance().getConfigReader(world);
+		if (actuator.isInProgress()) {
+			// If Blood Moon is in progress but its daytime, stop it
+			if (world.getTime() < 12000) {
 
-                if (daysBeforeBloodMoon == 1)
-                {
-                    LocaleReader.MessageAllLocale("BloodMoonTomorrow", null, null, world);
-                }
-                else
-                {
-                    LocaleReader.MessageAllLocale("DaysBeforeBloodMoon", new String[]{"$d"}, new String[]{String.valueOf(daysBeforeBloodMoon)}, world);
-                }
-                SetDaysRemaining(daysBeforeBloodMoon - 1);
-                checkupAfter = getNextEvening();
-                return;
-            }
+				actuator.StopBloodMoon();
+				LocaleReader.MessageAllLocale("BloodMoonEndingMessage", null, null, world);
+				for (Player player : world.getPlayers()) {
+					if (configReader.GetBloodMoonEndSoundConfig())
+						player.playSound(player.getLocation(), Sound.UI_TOAST_CHALLENGE_COMPLETE, 100.0f, 1.2f);
+				}
+				checkupAfter = 0; // This avoids some bugs when players use /time set 0
+			}
 
-            //Day 0: prepare for Blood Moon
-            LocaleReader.MessageAllLocale("BloodMoonTonight", null, null, world);
-            checkupAfter = getTodayZero() + 12000;
-        }
-    }
-    private void Check13 ()
-    {
-        LocaleReader localeReader = Bloodmoon.GetInstance().getLocaleReader();
-        ConfigReader configReader = Bloodmoon.GetInstance().getConfigReader(world);
-        //Check if its Blood Moon night, then time is over 13000, and if its the day after the day 0 warning
-        if (world.getFullTime() >= checkupAfter && world.getTime() >= 12000 && daysBeforeBloodMoon == 0)
-        {
+		}
 
-            actuator.StartBloodMoon();
+	}
 
-            checkupAfter = getNextEvening();
-            daysBeforeBloodMoon = (configReader.GetIntervalConfig() - 1);
-        }
-    }
-    private void CheckDay ()
-    {
-        LocaleReader localeReader = Bloodmoon.GetInstance().getLocaleReader();
-        ConfigReader configReader = Bloodmoon.GetInstance().getConfigReader(world);
-        if (actuator.isInProgress())
-        {
-            //If Blood Moon is in progress but its daytime, stop it
-            if (world.getTime() < 12000)
-            {
+	private long getNextEvening() {
+		long currentTime = world.getFullTime();
+		long remaining = currentTime % DAY;
 
-                actuator.StopBloodMoon();
-                LocaleReader.MessageAllLocale("BloodMoonEndingMessage", null, null, world);
-                for (Player player : world.getPlayers())
-                {
-                    if (configReader.GetBloodMoonEndSoundConfig())
-                        player.playSound(player.getLocation(), Sound.UI_TOAST_CHALLENGE_COMPLETE, 100.0f, 1.2f);
-                }
-                checkupAfter = 0; //This avoids some bugs when players use /time set 0
-            }
+		return ((currentTime - remaining) + DAY + 11000);
+	}
 
-        }
+	private long getTodayZero() {
+		long currentTime = world.getFullTime();
+		long remaining = currentTime % DAY;
 
-    }
+		return (currentTime - remaining);
+	}
 
-    private long getNextEvening ()
-    {
-        long currentTime = world.getFullTime();
-        long remaining = currentTime % DAY;
+	// We need to make sure the checkAt var is reset when time is manually changed
+	@EventHandler
+	public void onCommandIssued(PlayerCommandPreprocessEvent event) {
+		Player sender = event.getPlayer();
+		String command = event.getMessage();
 
-        return ((currentTime - remaining) + DAY + 11000);
-    }
-
-    private long getTodayZero ()
-    {
-        long currentTime = world.getFullTime();
-        long remaining = currentTime % DAY;
-
-        return (currentTime - remaining);
-    }
-
-
-    //We need to make sure the checkAt var is reset when time is manually changed
-    @EventHandler
-    public void onCommandIssued (PlayerCommandPreprocessEvent event)
-    {
-        Player sender = event.getPlayer();
-        String command = event.getMessage();
-
-        if (command.startsWith("/time set")
-            || command.startsWith("/night")
-            || command.startsWith("/day")
-        )
-        {
-            if (sender.getWorld() == world && !event.isCancelled())
-            {
-                SetCheckAfter(0);
-            }
-        }
-    }
+		if (command.startsWith("/time set") || command.startsWith("/night") || command.startsWith("/day")) {
+			if (sender.getWorld() == world && !event.isCancelled()) {
+				SetCheckAfter(0);
+			}
+		}
+	}
 }
